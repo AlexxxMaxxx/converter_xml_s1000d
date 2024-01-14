e04b1490534cafae3b925b6ee1a82865d795bb42
Anastasiia Maximova <anastasia_maxim@bk.ru>
2024-01-14 15:50:58 +0300

    Create component Uploader

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div>Work!</div>
+  <div class="app">
+    <h1>Конвертировать Word в XML S1000D</h1>
+    <Uploader></Uploader>
+    <span class="file-info">Файл: {{ uploaderFile.name }}</span>
+  </div>
 </template>
 
 <script>
-export default {
+import Uploader from '@/components/Uploader';
+import { ref } from 'vue';
 
+export default {
+  components: {
+    Uploader
+  },
+  setup() {
+    const uploaderFile = ref("");
+    
+    return {
+      uploaderFile
+    };
+  },
 }
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+<style lang="scss">
+  
 </style>
---
+++ src/components/Uploader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="uploader">
+     <span>Перетащите файл сюда</span>
+     <span>ИЛИ</span>
+     <label>
+      Выберите файл
+      <input 
+        type="file" 
+        name="uploaderFile"
+        accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"/>
+     </label>
+     
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Uploader",
+
+}
+</script>
+
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app{
+  padding: 20px;
+}
+
+.uploader {
+  margin: 0 auto;
+  padding: 15px;
+  width: 50%;
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px black dashed;
+  border-radius: 10px;
+  background-color: white;
+  transition: 0.3s ease all;
+
+  label {
+    padding: 8px 12px;
+    color: white;
+    background-color: #A63CD4;
+    transition: 0.3s ease all;
+
+    input {
+      display: none;
+    }
+  }
+}
+</style>
